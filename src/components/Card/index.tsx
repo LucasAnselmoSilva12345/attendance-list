111339c2042ad6960da11d8131d87e2a5cc0ae6d
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-07-16 19:36:58 -0300

    Feat: Change styles from CSS to Tailwind styles
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,3 @@
-import './styles.css';
-
 export type CardProps = {
   name: string;
   time: string;
@@ -7,8 +5,8 @@ export type CardProps = {
 
 export function Card({ name, time }: CardProps) {
   return (
-    <div className="container-card">
-      <strong>{name}</strong>
+    <div className="w-3/4 md:w-1/2 h-100 mb-5 p-5 rounded-10 flex items-center justify-between bg-astral-700 text-white">
+      <strong className="text-lg">{name}</strong>
       <small>{time}</small>
     </div>
   );
