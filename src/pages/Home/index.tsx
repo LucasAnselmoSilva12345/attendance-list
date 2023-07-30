50edcb0e488e77560d7b26ff14a551bcd86b4fba
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-07-30 17:49:37 -0300

    Feat: Input name component and test Fixe: Refactor home component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,14 @@
-import { useState } from 'react';
-import { Card, CardProps } from '../../components/Card';
 import { ProfileGitHub } from '../../components/ProfileGitHub';
+import { InputName } from '../../components/InputName';
 
 export function Home() {
-  const [studentName, setStudentName] = useState('');
-  const [students, setStudents] = useState<CardProps[]>([]);
-
-  function handleAddStudent() {
-    const newStudent = {
-      name: studentName,
-      time: new Date().toLocaleTimeString('pt-br', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-      }),
-    };
-
-    setStudents((rest) => [...rest, newStudent]);
-  }
-
   return (
     <div className="flex flex-col items-center">
       <header className="w-3/4 md:w-1/2 mt-84 mx-0 mb-6 flex items-center justify-between">
         <h1 className="text-2xl font-bold text-astral-950">Attendance list</h1>
         <ProfileGitHub />
       </header>
-      <input
-        className="w-3/4 md:w-1/2 p-6 rounded-md border-none bg-astral-50"
-        type="text"
-        placeholder="Enter your name..."
-        onChange={(e) => setStudentName(e.target.value)}
-      />
-      <button
-        className="w-3/4 md:w-1/2 mt-3 mx-0 mb-84 p-6 font-bold rounded-md border-none cursor-pointer transition-opacity bg-astral-600 text-white hover:opacity-80"
-        type="button"
-        onClick={handleAddStudent}
-      >
-        Send
-      </button>
-
-      {students.map((student) => (
-        <Card key={student.time} name={student.name} time={student.time} />
-      ))}
+      <InputName />
     </div>
   );
 }
